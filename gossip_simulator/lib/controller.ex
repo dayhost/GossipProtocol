c04de8c503ad4fbef77b4025c2d560bd6c728f9f
dayhost <dayhost@users.noreply.github.com>
2017-10-03 22:03:42 -0400

    do some change in the main funtion

--- gossip_simulator/lib/controller.ex
+++ gossip_simulator/lib/controller.ex
@@ -1,3 +1,16 @@
 defmodule GossipSimulator.Controller do
-    def 
+    def createTopology(nums) do
+        pidList = []
+        {:ok, pid} = GossipSimulator.Node.start_link()
+        pidList = [pidList | pid]
+        {:ok, pid} = GossipSimulator.Node.start_link()
+        pidList = [pidList | pid]
+    end
+
+    def trigger(pidList) do
+        [pid1 | pid2] = pidList
+        GossipSimulator.Node.setNeighbor(pid1, [pid2])
+        GossipSimulator.Node.setNeighbor(pid2, [pid1])
+        GossipSimulator.Node.send(pid1, "aaa")
+    end
 end
